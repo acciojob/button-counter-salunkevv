17d3d47718f9025b83d671e0d45e8d486c6c1c74
salunkevv <86159929+salunkevv@users.noreply.github.com>
2024-01-14 18:25:25 +0000

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let[a,setA]=useState(0)
+  function func()
+  {
+    setA(a+1)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {a} times</p>
+        <button onClick={func}>Click me</button>
     </div>
   )
 }
